a91daf209e9db6e71142cdf9db1b4558cfc44d72
Edward Robinson <me@eddrobinson.net>
2012-11-18 00:52:56 +0000

    methods for finding by postcode or (lat, lng)

--- boris.py
+++ boris.py
@@ -1,7 +1,9 @@
 # -- coding: utf-8 --
 import datetime
 from math import sin, cos, atan2, sqrt, radians
+
 from lxml import etree
+from postcodes import PostCoder
 
 # Configuration variables
 TFL_DATA_LOC = "http://www.tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml"
@@ -48,6 +50,8 @@ def _haversine(first, second):
     """
     Calculate the `haversine`_ distance between two points on a sphere.
 
+    .. _haversine: http://en.wikipedia.org/wiki/Haversine_formula
+
     :param first: the first (latitude, longitude) tuple.
 
     :param second: the second (latitude, longitude) tuple.
@@ -67,16 +71,14 @@ def _haversine(first, second):
     R - is the Earth's radius in km (we will use 6371 km)
 
     :returns: the distance in km between two points.
-
-    .. _haversine: http://en.wikipedia.org/wiki/Haversine_formula
     """
     lat_1 = radians(first[0])
     lat_2 = radians(second[0])
     d_lat = radians(second[0] - first[0])
     d_lng = radians(second[1] - first[1])
     R = 6371.0
-    a = pow(sin(d_lat / 2.0), 2) + cos(lat_1) * \
-        cos(lat_2) * pow(sin(d_lng / 2.0), 2)
+    a = pow(sin(d_lat / 2.0), 2) + cos(lat_1) * cos(lat_2) * \
+        pow(sin(d_lng / 2.0), 2)
     c = 2.0 * atan2(sqrt(a), sqrt(1 - a))
     return R * c
 
@@ -99,8 +101,19 @@ class BikeChecker(object):
     :returns: a list of dictionaries containing bike station data
     """
 
-    def _process_stations(self, stations):
+    def __init__(self, endpoint=None):
+        self.pc = PostCoder()
+        self._last_updated = 0
+        self._etree = None
+        self._stations_lst = []
+        self._stations_map = {}
+        self.endpoint = endpoint or TFL_DATA_LOC
+
+    def _process_stations(self):
+        stations = _parse_feed(self.endpoint)
         self._stations_lst = [dict(_convert(e) for e in st) for st in stations] 
+        if not self._stations_lst:
+            raise InvalidDataException("No Station data available")
 
     def all(self, skip_cache=False):
         """
@@ -115,12 +128,53 @@ class BikeChecker(object):
         """
         now = _time_ms(datetime.datetime.utcnow())
         if skip_cache or now - self._last_updated > CACHE_LIMIT:
-            self._process_stations(_parse_feed(self.endpoint))
+            self._process_stations()
         return self._stations_lst
 
-    def __init__(self, endpoint=None):
-        self._last_updated = 0
-        self._etree = None
-        self._stations_lst = []
-        self._stations_map = {}
-        self.endpoint = endpoint or TFL_DATA_LOC
+    def find_with_geo(self, lat, lng):
+        """
+        Availability information for the nearest station to 
+        (`lat`, `lng`).
+
+        :param lat: latidude of position
+
+        :param lng: longitude of position
+
+        :returns: a `dict` containing availability at the nearest station
+        """
+        if not self._stations_lst:
+            self._process_stations()
+        near, near_dist = None, None
+        for station in self._stations_lst:
+            st_geo = (station['geo']['lat'], station['geo']['lng'])
+            station_dist = _haversine((lat, lng), st_geo)
+            if not near_dist or station_dist < near_dist:
+                near, near_dist = station, station_dist
+        return near
+
+    def find_with_postcode(self, postcode):
+        """ 
+        Availability information for the nearest station to `postcode`.
+
+        :param postcode: the postcode to search
+
+        :returns: a `dict` containing availability at the nearest station
+        """
+        info = self.pc.get(postcode)
+        if not info:
+            raise InvalidPostcodeException("No known postcode %s" % postcode)
+        if 'geo' not in info or not set(['lat', 'lng']) <= set(info['geo']):
+            raise InvalidDataException("Missing latitude and/or longitude")
+        lat, lng = info['geo']['lat'], info['geo']['lng']
+        return self.find_with_geo(lat, lng)
+
+
+class StationDataException(Exception):
+    pass
+
+class InvalidDataException(Exception):
+    pass
+
+class InvalidPostcodeException(Exception):
+    pass
+        
